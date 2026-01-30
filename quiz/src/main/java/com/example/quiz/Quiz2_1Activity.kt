14823adec5520a1e44afc58fc19b2c7ff9f476fc
juu124 <juu12424@gmail.com>
2026-01-30 22:00:11 +0900

    [ch4 : event 과제] - 회원가입 이벤트 추가 및 고도화

--- quiz/src/main/java/com/example/quiz/Quiz2_1Activity.kt
+++ quiz/src/main/java/com/example/quiz/Quiz2_1Activity.kt
@@ -1,19 +1,22 @@
 package com.example.quiz
 
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.util.Patterns
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.quiz.databinding.ActivityQuiz21Binding
+import java.util.regex.Pattern
 
+//typealias patterns = java.util.Patterns
 class Quiz2_1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         val binding = ActivityQuiz21Binding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -22,34 +25,106 @@ class Quiz2_1Activity : AppCompatActivity() {
             insets
         }
 
-        val name = binding.etName.text
-        val email = binding.etEmail.text
+        binding.run {
+            checkAgree.setOnCheckedChangeListener { buttonView, isChecked ->
+                btnJoin.isEnabled = isChecked
+            }
 
-        binding.btnJoin.setOnClickListener {
-            if (name.isEmpty()) {
-                Toast.makeText(this, "이름을 입력해주세요", Toast.LENGTH_SHORT).show()
-            } else if (email.isEmpty()) {
-                Toast.makeText(this, "이메일을 입력해주세요", Toast.LENGTH_SHORT).show()
-            } else if (!binding.checkAgree.isChecked) {
-                Toast.makeText(this, "이용약관에 동의해주세요", Toast.LENGTH_SHORT).show()
-            } else {
-                binding.tvResultTitle.visibility = View.VISIBLE
-                binding.tvResult.visibility = View.VISIBLE
+            btnJoin.setOnClickListener {
+                if (checkAgree.isChecked)
+                    if (binding.etName.text.isEmpty()) {
+                        showToast("name invalid")
+                        return@setOnClickListener   // break 배울때 사용했던 라벨 사용. 해당 listener 문을 나가게 해준다.
+                    }
 
-                val gender = if (binding.rbMale.isChecked) "남성" else "여성"
-                val result = """
-                    이름 : $name
-                    이메일 : $email
-                    성별 : $gender
-                    """.trimIndent()
-                binding.tvResult.text = result
+                val email = binding.etEmail.text.toString()
+                if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    showToast("email invalid")
+                    return@setOnClickListener
+                }
+
+                // 출력 Log로 표시
+                printLog(binding)
+
+                // 가입하기 버튼 활성화
+                btnJoin.isEnabled = true
 
                 // 입력창 초기화
-                binding.etName.text.clear()
-                binding.etEmail.text.clear()
-                binding.rbMale.isChecked = true
-                binding.checkAgree.isChecked = false
+                initState(binding)
+
+                // hof도 사용했는데 효과적이지 않아서 주석처리
+                // 이름 empty check
+//                checkEmptyEditText(binding.etName, "이름을 입력해주세요") {
+//                    // 이메일 empty check
+//                    checkEmptyEditText(binding.etEmail, "이메일을 입력해주세요") {
+//                        // 이메일 입력 형식 체크
+//                        checkInputFormat(
+//                            binding.etEmail,
+//                            Patterns.EMAIL_ADDRESS,
+//                            "이메일 형식이 올바르지 않습니다."
+//                        ) {
+//                            // 출력 Log로 표시
+//                            printLog(binding)
+//
+//                            // 가입하기 버튼 활성화
+//                            btnJoin.isEnabled = true
+//
+//                            // 입력창 초기화
+//                            initState(binding)
+//                        }
+//                    }
+//                }
             }
         }
     }
+
+    // 토스트 노출
+    fun showToast(message: String) {
+        Toast.makeText(this@Quiz2_1Activity, message, Toast.LENGTH_SHORT).show()
+    }
+
+    // 입력창 초기화
+    fun initState(binding: ActivityQuiz21Binding) {
+        binding.run {
+            etName.text.clear()
+            etEmail.text.clear()
+            rbMale.isChecked = true
+            checkAgree.isChecked = false
+        }
+    }
+
+    // 출력 Log로 표시
+    fun printLog(binding: ActivityQuiz21Binding) {
+        val name = binding.etName.text.toString()
+        val email = binding.etEmail.text.toString()
+        val gender = if (binding.rbMale.isChecked) "남성" else "여성"
+        val result = "이름 : $name, 이메일 : $email, 성별 : $gender"
+        Log.d("Quiz2_1Activity", result)
+    }
+
+
+
+    // hof도 사용했는데 효과적이지 않아서 주석처리
+//    fun checkEmptyEditText(editText: EditText, errorMessage: String, onSuccess: () -> Unit) {
+//        val result = editText.text.toString()
+//        if (result.isEmpty()) {
+//            showToast(errorMessage)
+//        } else {
+//            onSuccess()
+//        }
+//    }
+//
+//    fun checkInputFormat(
+//        editText: EditText,
+//        patterns: Pattern,
+//        errorMessage: String,
+//        onSuccess: () -> Unit
+//    ) {
+//        val result = editText.text.toString()
+//        if (!patterns.matcher(result).matches()) {
+//            showToast(errorMessage)
+//        } else {
+//            onSuccess()
+//        }
+//    }
 }
