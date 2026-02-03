c2db9bd605d162a376c16ecc6a2bcd26b4472d2f
juu124 <juu12424@gmail.com>
2026-02-03 11:10:06 +0900

    [ch7 : permission 실습] - ch7 퍼미션 없이 두 앱 상호작용

--- ch7/src/main/java/com/example/ch7/Test1Activity.kt
+++ ch7/src/main/java/com/example/ch7/Test1Activity.kt
@@ -1,20 +1,43 @@
 package com.example.ch7
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ch7.databinding.ActivityTest1Binding
 
 class Test1Activity : AppCompatActivity() {
+    lateinit var binding: ActivityTest1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_test1)
+
+        binding = ActivityTest1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.button1.setOnClickListener {
+            // 액티비티 실행
+            val intent = Intent("CH7_ACTION_ONE")
+            startActivity(intent)
+        }
+        binding.button2.setOnClickListener {
+            // 액티비티 실행
+            val intent = Intent("CH7_ACTION_TWO")
+            startActivity(intent)
+        }
+        binding.button3.setOnClickListener {
+            // 액티비티 실행
+            val intent = Intent("CH7_ACTION_THREE")
+            startActivity(intent)
         }
     }
 }
