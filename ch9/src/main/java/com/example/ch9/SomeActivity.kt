1f5d021059b982d81514705eb03d1d703044aa8b
juu124 <juu12424@gmail.com>
2026-02-05 10:15:04 +0900

    [ch9 : ActivityResultLauncher  실습] - ActivityResultLauncher  실습

---
+++ ch9/src/main/java/com/example/ch9/SomeActivity.kt
@@ -0,0 +1,38 @@
+package com.example.ch9
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ch9.databinding.ActivitySomeBinding
+
+class SomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val binding = ActivitySomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 자신을 실행시킨 intent 에서 데이터를 추출한다.
+        val data1 = intent.getStringExtra("data1")
+        val data2 = intent.getIntExtra("data2", 0)
+        binding.dataView.text = "$data1, $data2"
+
+        binding.button.setOnClickListener {
+            // 데이터를 포함해서 자신을 종료시켜 자동으로 이전화면으로 넘어가게 한다.
+            // 이때 결과 데이터도 넘기겠다.
+            intent.putExtra("result", "hello world")
+            // 되돌리는 상태를 표시해야한다. 어떤 상태로 되돌리는 지 표시해야한다.
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+}
--- ch9/src/main/java/com/example/ch9/Test2Activity.kt
+++ ch9/src/main/java/com/example/ch9/Test2Activity.kt
@@ -1,20 +1,43 @@
 package com.example.ch9
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ch9.databinding.ActivityTest2Binding
 
 class Test2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_test2)
+        val binding = ActivityTest2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // intent가 되돌아 왔을때 사후 처리를 하는 케이스
+        // launcher방법을 사용
+        val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            // callback 되돌아 왔을대 자동 실행된다.
+            // 결과 데이터 획득
+            val intent = it.data
+            binding.dataView.text = intent?.getStringExtra("result")
+        }
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, SomeActivity::class.java)
+            intent.putExtra("data1", "hello")
+            intent.putExtra("data2", 100)
+            // intent를 실행시키는 방법이 startActivity말고 launch도 있다.
+            // 이렇게 하면 사후 결과 데이터를 획득할 수도 있음
+            launcher.launch(intent)
         }
     }
 }
